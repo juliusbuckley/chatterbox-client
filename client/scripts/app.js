18f0f2f5148e605e834218a4a1fecac21a214d65
juliusbuckley <juliusbuckley@gmail.com>
2016-06-20 15:56:28 -0700

    Completed getMessages and displayMessages functions.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,36 @@
-// YOUR CODE HERE:
+var getMessages = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages', 
+    type: 'GET',
+    contentType: 'application/JSON',
+    success: function(result) {
+      var messages = result['results'];
 
+      displayMessages(messages);
+
+
+
+      // console.log('Messages received.');
+    },
+    error: function(data) {
+      console.log('failed message', data);
+    }
+  });
+};
+
+getMessages();
+
+var displayMessages = function(messageArray) {
+  _.each(messageArray, function(message) {
+    var text = 'Message: ' + message.text;
+    var user = 'User: ' + message.username;
+
+    var $messageText = $('<div class="message"></div>');
+    var $ul = $('<ul></ul>');
+    var $lineUser = $('<li class="user"></li>').text(message.user).appendTo($ul);
+    var $lineText = $('<li></li>').text(message.text).appendTo($ul);
+
+    $ul.appendTo($messageText);
+    $messageText.appendTo($('#chats'));
+  });
+};
