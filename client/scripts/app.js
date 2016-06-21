f23c76e154ad67e87a62c9a1f9c2e4883d71a299
juliusbuckley <juliusbuckley@gmail.com>
2016-06-20 17:09:15 -0700

    Implemented postMessages and createMessages functions. Both working successfully.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,9 @@
+var message1 = {
+  username: 'Superman',
+  text: 'Where is Lex Luthor?',
+  roomname: 'Metropolis'
+};
+
 var getMessages = function() {
   $.ajax({
     url: 'https://api.parse.com/1/classes/messages', 
@@ -5,32 +11,91 @@ var getMessages = function() {
     contentType: 'application/JSON',
     success: function(result) {
       var messages = result['results'];
-
       displayMessages(messages);
-
-
-
-      // console.log('Messages received.');
     },
     error: function(data) {
-      console.log('failed message', data);
+      console.log('What the French, toast?! The messages didn\'t load!', data);
     }
   });
 };
 
-getMessages();
+var postMessage = function(message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
 
 var displayMessages = function(messageArray) {
+  var $chats = $('#chats');
+
+  // removes all messages before refresh
+  $chats.empty();
+
+  // add messages to #chats
   _.each(messageArray, function(message) {
     var text = 'Message: ' + message.text;
     var user = 'User: ' + message.username;
+    var time = 'Time: ' + message.updatedAt;
 
     var $messageText = $('<div class="message"></div>');
     var $ul = $('<ul></ul>');
-    var $lineUser = $('<li class="user"></li>').text(message.user).appendTo($ul);
-    var $lineText = $('<li></li>').text(message.text).appendTo($ul);
+    var $lineUser = $('<li class="user"></li>').text(user).appendTo($ul);
+    var $lineText = $('<li class="text"></li>').text(text).appendTo($ul);
+    var $lineTime = $('<li class="time"></li>').text(time).appendTo($ul);
 
     $ul.appendTo($messageText);
-    $messageText.appendTo($('#chats'));
+    $messageText.prependTo($chats);
   });
 };
+
+var createMessage = function() {
+  var $user = $('#user').val();
+  var $text = $('#userMessage').val();
+
+  var messageObject = {};
+  messageObject['username'] = $user;
+  messageObject['text'] = $text;
+  messageObject['roomname'] = 'Gotham';
+
+  postMessage(messageObject);
+};
+
+// var getRoomNames = function() {
+//   $.ajax({
+//     url: 'https://api.parse.com/1/classes/messages', 
+//     type: 'GET',
+//     contentType: 'application/JSON',
+//     success: function(result) {
+//       var messages = result['results'];
+//       var roomnameObject = {};
+
+
+
+//     },
+//     error: function(data) {
+//       console.log('What the French, toast?! The messages didn\'t load!', data);
+//     }
+//   });
+// };
+
+
+
+
+
+
+getMessages();
+
+setInterval(function() {
+  getMessages();
+  console.log('refreshed!');  
+}, 5000);
+
